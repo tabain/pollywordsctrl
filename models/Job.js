029fcc285ed254ab84ad1a9d1db2d345b35abb66
tabain <1taabi@gmail.com>
2014-10-09 14:38:13 +0000

    modify

---
+++ models/Job.js
@@ -0,0 +1,21 @@
+var mongoose = require("mongoose");
+
+var jobSchema = mongoose.Schema({
+   title:{type:String},
+   description:{type:String}
+});
+
+var Job = mongoose.model('Job', jobSchema);
+
+exports.seedJobs = function(){
+    Job.find({}).exec(function(error, collection){
+        
+        if(collection.length === 0){
+            Job.create({title:'Cook', description:'You will be making'});
+            Job.create({title:'Waiter', description:'You will be making'});
+            Job.create({title:'Programmer', description:'You will be making'});
+            Job.create({title:'Axe Maker', description:'You will be making'});
+        }
+        
+    })
+}
--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 var express = require("express");
+var mongoose = require("mongoose");
+var jobModel = require("./models/Job")
 var app = express();
 
 
 app.set('views', __dirname);
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
-
+app.get('/api/jobs', function(req, res) {
+    mongoose.model('Job').find({}).exec(function(error, collection){
+        res.send(collection);
+    })
+    
+})
 app.get('*', function(req, res){
    res.render('index'); 
+});
+
+// mongoose.connect('mongodb://localhost/pollywordsapi');
+mongoose.connect('mongodb://api:admin@ds043180.mongolab.com:43180/pollywordsapi'); 
+var conn = mongoose.connection;
+conn.once('open', function(){
+    console.log('connect to mongodb successfully')
+    jobModel.seedJobs();
 });
 
 app.listen(process.env.PORT, process.env.IP);
